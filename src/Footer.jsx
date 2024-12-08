37e4c6473cc055e5ef97969694c1ef57c141adad
DeepthaMaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2024-12-07 17:19:01 -0800

    Delete src directory

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-50">
-      <div className="px-16 py-4 flex justify-center items-center text-base font-bold text-gray-950">
-        &copy; {new Date().getFullYear()}. All rights reserved by Deeptha
-      </div>
-    </footer>
-  );
-}
